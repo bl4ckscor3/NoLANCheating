ef9554bf2e6a278085b6bb70ff35e9b35a0051c8
bl4ckscor3 <bl4ckscor33@gmail.com>
2019-08-11 13:38:25 +0200

    Initial commit

---
+++ src/main/java/bl4ckscor3/mod/nolancheating/NoLANCheating.java
@@ -0,0 +1,27 @@
+package bl4ckscor3.mod.nolancheating;
+
+import net.minecraft.client.gui.screen.ShareToLanScreen;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.GuiScreenEvent.InitGuiEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@Mod(NoLANCheating.MODID)
+@EventBusSubscriber(modid=NoLANCheating.MODID, value=Dist.CLIENT)
+public class NoLANCheating
+{
+	public static final String MODID = "nolancheating";
+
+	@SubscribeEvent
+	public static void onGuiInit(InitGuiEvent.Post event)
+	{
+		if(event.getGui() instanceof ShareToLanScreen)
+		{
+			ShareToLanScreen screen = (ShareToLanScreen)event.getGui();
+
+			screen.allowCheatsButton.visible = false;
+			screen.gameModeButton.x = screen.width / 2 - screen.gameModeButton.getWidth() / 2;
+		}
+	}
+}
