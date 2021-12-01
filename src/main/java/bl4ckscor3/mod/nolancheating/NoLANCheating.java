988db1c9dd357ad12d3250a4dfc6dbd6c9063f61
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-12-01 12:51:53 +0100

    update to 1.18

--- src/main/java/bl4ckscor3/mod/nolancheating/NoLANCheating.java
+++ src/main/java/bl4ckscor3/mod/nolancheating/NoLANCheating.java
@@ -3,7 +3,7 @@ package bl4ckscor3.mod.nolancheating;
 import net.minecraft.client.gui.components.AbstractButton;
 import net.minecraft.client.gui.screens.ShareToLanScreen;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.GuiScreenEvent.InitGuiEvent;
+import net.minecraftforge.client.event.ScreenEvent.InitScreenEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -15,11 +15,11 @@ public class NoLANCheating
 	public static final String MODID = "nolancheating";
 
 	@SubscribeEvent
-	public static void onGuiInit(InitGuiEvent.Post event)
+	public static void onGuiInit(InitScreenEvent.Post event)
 	{
-		if(event.getGui() instanceof ShareToLanScreen)
+		if(event.getScreen() instanceof ShareToLanScreen)
 		{
-			ShareToLanScreen screen = (ShareToLanScreen)event.getGui();
+			ShareToLanScreen screen = (ShareToLanScreen)event.getScreen();
 			AbstractButton commandsButton = (AbstractButton)screen.renderables.get(1);
 			AbstractButton modeButton = (AbstractButton)screen.renderables.get(0);
 
