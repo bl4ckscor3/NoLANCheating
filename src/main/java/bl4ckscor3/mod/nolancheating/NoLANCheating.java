00b362cf503f3baf583e6c4662da7e3283d9c62e
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-07-08 21:23:36 +0200

    fix compatibility with Forge 41.0.64+

--- src/main/java/bl4ckscor3/mod/nolancheating/NoLANCheating.java
+++ src/main/java/bl4ckscor3/mod/nolancheating/NoLANCheating.java
@@ -3,7 +3,7 @@ package bl4ckscor3.mod.nolancheating;
 import net.minecraft.client.gui.components.AbstractButton;
 import net.minecraft.client.gui.screens.ShareToLanScreen;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.ScreenEvent.InitScreenEvent;
+import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -15,7 +15,7 @@ public class NoLANCheating
 	public static final String MODID = "nolancheating";
 
 	@SubscribeEvent
-	public static void onGuiInit(InitScreenEvent.Post event)
+	public static void onGuiInit(ScreenEvent.Init.Post event)
 	{
 		if(event.getScreen() instanceof ShareToLanScreen screen)
 		{
