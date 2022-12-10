a4346ad83f1a4ada2817f25825b0754ecafb7953
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-12-10 16:31:34 +0100

    apply code formatting

--- src/main/java/bl4ckscor3/mod/nolancheating/NoLANCheating.java
+++ src/main/java/bl4ckscor3/mod/nolancheating/NoLANCheating.java
@@ -9,18 +9,15 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
 @Mod(NoLANCheating.MODID)
-@EventBusSubscriber(modid=NoLANCheating.MODID, value=Dist.CLIENT)
-public class NoLANCheating
-{
+@EventBusSubscriber(modid = NoLANCheating.MODID, value = Dist.CLIENT)
+public class NoLANCheating {
 	public static final String MODID = "nolancheating";
 
 	@SubscribeEvent
-	public static void onGuiInit(ScreenEvent.Init.Post event)
-	{
-		if(event.getScreen() instanceof ShareToLanScreen screen)
-		{
-			AbstractButton commandsButton = (AbstractButton)screen.renderables.get(1);
-			AbstractButton modeButton = (AbstractButton)screen.renderables.get(0);
+	public static void onGuiInit(ScreenEvent.Init.Post event) {
+		if (event.getScreen() instanceof ShareToLanScreen screen) {
+			AbstractButton commandsButton = (AbstractButton) screen.renderables.get(1);
+			AbstractButton modeButton = (AbstractButton) screen.renderables.get(0);
 
 			commandsButton.visible = false;
 			modeButton.x = screen.width / 2 - modeButton.getWidth() / 2;
