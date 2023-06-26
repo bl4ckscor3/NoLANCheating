dd5f385b3004f26c9cb9dcd1d031c55bf51533e6
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-06-26 23:12:44 +0200

    add option to remove the allow cheats button when creating a world
    - update to use settings.gradle
    - add config

--- src/main/java/bl4ckscor3/mod/nolancheating/NoLANCheating.java
+++ src/main/java/bl4ckscor3/mod/nolancheating/NoLANCheating.java
@@ -1,26 +1,57 @@
 package bl4ckscor3.mod.nolancheating;
 
+import org.apache.commons.lang3.tuple.Pair;
+
 import net.minecraft.client.gui.components.AbstractButton;
 import net.minecraft.client.gui.screens.ShareToLanScreen;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ScreenEvent;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.config.ModConfig;
 
 @Mod(NoLANCheating.MODID)
 @EventBusSubscriber(modid = NoLANCheating.MODID, value = Dist.CLIENT)
 public class NoLANCheating {
 	public static final String MODID = "nolancheating";
+	public static final ForgeConfigSpec CONFIG_SPEC;
+	public static final Config CONFIG;
+
+	static {
+		Pair<Config, ForgeConfigSpec> clientSpecPair = new ForgeConfigSpec.Builder().configure(Config::new);
+
+		CONFIG_SPEC = clientSpecPair.getRight();
+		CONFIG = clientSpecPair.getLeft();
+		ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, CONFIG_SPEC);
+	}
 
 	@SubscribeEvent
 	public static void onGuiInit(ScreenEvent.Init.Post event) {
-		if (event.getScreen() instanceof ShareToLanScreen screen) {
+		if (event.getScreen() instanceof ShareToLanScreen screen && CONFIG.removeButtonInOpenToLANScreen.get()) {
 			AbstractButton commandsButton = (AbstractButton) screen.renderables.get(1);
 			AbstractButton modeButton = (AbstractButton) screen.renderables.get(0);
 
 			commandsButton.visible = false;
 			modeButton.setX(screen.width / 2 - modeButton.getWidth() / 2);
+		}
+	}
+
+	public static class Config {
+		public BooleanValue removeButtonInOpenToLANScreen, removeButtonInCreateNewWorldScreen;
+
+		Config(ForgeConfigSpec.Builder builder) {
+			//@formatter:off
+			removeButtonInOpenToLANScreen = builder
+					.comment("Removes the \"Allow Cheats\" button in the \"Open to LAN\" screen, if set to \"true\".")
+					.define("removeButtonInOpenToLANScreen", true);
+
+			removeButtonInCreateNewWorldScreen = builder
+					.comment("Removes the \"Allow Cheats\" button in the \"Create New World\" screen, if set to \"true\".")
+					.define("removeButtonInCreateNewWorldScreen", false);
 		}
 	}
 }
---
+++ src/main/java/bl4ckscor3/mod/nolancheating/mixin/GameTabMixin.java
@@ -0,0 +1,24 @@
+package bl4ckscor3.mod.nolancheating.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import bl4ckscor3.mod.nolancheating.NoLANCheating;
+import net.minecraft.client.gui.components.CycleButton;
+import net.minecraft.client.gui.layouts.GridLayout;
+import net.minecraft.client.gui.layouts.LayoutSettings;
+import net.minecraft.client.gui.screens.worldselection.CreateWorldScreen;
+import net.minecraft.client.gui.screens.worldselection.WorldCreationUiState;
+import net.minecraft.world.Difficulty;
+
+@Mixin(targets = "net.minecraft.client.gui.screens.worldselection.CreateWorldScreen$GameTab")
+public class GameTabMixin {
+	@Inject(method = "<init>", at = @At("TAIL"), locals = LocalCapture.CAPTURE_FAILSOFT)
+	private void nolancheating$maybeRemoveAllowCheatsButton(CreateWorldScreen this$0, CallbackInfo ci, GridLayout.RowHelper gridlayout$rowhelper, LayoutSettings layoutsettings, GridLayout.RowHelper gridlayout$rowhelper1, CycleButton<WorldCreationUiState.SelectedGameMode> cyclebutton, CycleButton<Difficulty> cyclebutton1, CycleButton<Boolean> cyclebutton2) {
+		if (NoLANCheating.CONFIG.removeButtonInCreateNewWorldScreen.get())
+			cyclebutton2.visible = false;
+	}
+}
